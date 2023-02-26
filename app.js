821174bfe43faf69764f0c48b2736dd1e3d5cb99
Iris Vonberg <irisavonberg@gmail.com>
2023-02-26 15:44:04 -0800

    Arrays

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-const friend = {
-    name : 'Laurence Svekis',
-    age : 40,
-    gender : 'male',
-    location : {
-        city : "Toronto",
-        province : "Ontario",
-        country: "Canada",
-        address : "100 Main Street"
-    }
+// ***CREATING OBJECTS***
+
+// const friend = {
+//     name : 'Laurence Svekis',
+//     age : 40,
+//     gender : 'male',
+//     location : {
+//         city : "Toronto",
+//         province : "Ontario",
+//         country: "Canada",
+//         address : "100 Main Street"
+//     }
+// }
+
+// const friendLocation = friend.location.country;
+// document.querySelector('.output').textContent = friendLocation;
+
+// console.log(friend);
+// console.log(friend.name);
+// console.log(friend['name']);
+
+// ***ARRAYS***
+
+const friends = ['Laurence', 'John', 'Steve', 'Linda', 'Jane'];
+console.log(friends);
+console.log(friends[2]);
+
+const friend1 = {
+    name: 'Laurence',
+};
+
+const friend2 = {
+    name: 'John',
+};
+
+const friendList = [friend1, friend2];
+friendList[10] = {
+    name: 'Linda'
 }
+console.log(friendList);
 
-const friendLocation = friend.location.country;
-document.querySelector('.output').textContent = friendLocation;
-
-console.log(friend);
-console.log(friend.name);
-console.log(friend['name']);
+document.querySelector('.output').textContent = friendList[1].name;
