dcf409cd38ab09f944eb94aeb072ce83f96096b5
Iris Vonberg <irisavonberg@gmail.com>
2023-02-27 15:47:00 -0800

    Iterating on Objects

--- app.js
+++ app.js
@@ -40,3 +40,90 @@
 // console.log(friendList);
 
 // document.querySelector('.output').textContent = friendList[1].name;
+
+// ***ITERATING ON ARRAYS***
+
+// const friend1 = {
+//     name: "Laurence"
+// };
+
+// const friend2 = {
+//     name: "John"
+// };
+
+// const friend3 = {
+//     name: "Jane"
+// };
+
+// const friends = [friend1, friend2];
+// friends.push(friend3);
+// console.log(friends);
+
+// for (let x=0; x<friends.length; x++) {
+//     console.log(friends[x]);
+// }
+
+// friends.forEach(function(item,index,array) {
+//     console.log(item.name);
+// })
+
+// for (ind in friends) {
+//     console.log(ind);
+// }
+//  const friendList = ['Laurence', 'John', 'Steve', 'Linda', 'Jane'];
+//  const newArray = [];
+
+// friendList.forEach(function(item) {
+//     console.log(item);
+//     let temp = {
+//         name : item
+//     }
+//     newArray.push(temp);
+// })
+// console.log(newArray);
+
+// *** ITERATE ON OBJECTS ***
+
+ const friend1 = {
+     name: "Laurence"
+ };
+
+ const friend2 = {
+    name: "John"
+};
+
+const friend3 = {
+    name: "Jane"
+ };
+
+ const friend = {
+    name : 'Laurence Svekis',
+    age : 40,
+    gender : 'male',
+    location : {
+        city : "Toronto",
+        province : "Ontario",
+        country: "Canada",
+        address : "100 Main Street"
+    }
+}
+
+const friends = [friend1, friend2, friend3];
+console.log(friends);
+
+for (const key in friends) {
+    console.log(friends[key]);
+}
+
+console.log(Object.entries(friend));
+for(const [key,value] of Object.entries(friends)) {
+    console.log(key);
+    console.log(value);
+    console.log(value.name);
+}
+
+for (const [key] of Object.entries(friends)) {
+    friends[key].lastName = 'Smith' 
+}
+
+console.log(friends);
